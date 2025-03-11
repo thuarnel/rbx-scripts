d318899321f55805d84352f36d857324ce90f46a
thuarnel <196966936+thuarnel@users.noreply.github.com>
2025-03-10 18:49:26 -0700

    key system example

---
+++ scripts/rbx-placeholder-keyscript.lua
@@ -0,0 +1,6 @@
+return function(key)
+    if key == 'yes' then
+        print('omg you found the key')
+    end
+    return false
+end
--- scripts/rbx-voltex-keysys.lua
+++ scripts/rbx-voltex-keysys.lua
@@ -5,6 +5,8 @@ if type(env.stop_voltex_key_system) == 'function' then
 	env.stop_voltex_key_system()
 end
 
+local wait = task.wait
+
 local instances = {}
 local connections = {}
 
@@ -20,88 +22,125 @@ local setclipboard = env.setclipboard
 
 --[=[ BEGIN SCRIPT ]=]--
 
-local players = game:GetService('Players')
-local coregui = game:GetService('CoreGui')
-local localplayer = players.LocalPlayer
+local key_ui = Instance.new("ScreenGui")
+key_ui.Name = "key_ui"
+key_ui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-local ScreenGui = Instance.new("ScreenGui", coregui)
-table.insert(instances, ScreenGui)
+local frame = Instance.new("Frame")
+frame.Name = "frame"
+frame.AnchorPoint = Vector2.new(0.5, 0.5)
+frame.BackgroundColor3 = Color3.new(0.0627, 0.0627, 0.0627)
+frame.Position = UDim2.new(0.5, 0, 0.5, 0)
+frame.BorderColor3 = Color3.new()
+frame.BorderSizePixel = 0
+frame.Size = UDim2.new(0.3, 0, 0.3, 0)
+frame.Parent = key_ui
 
-local Frame = Instance.new("Frame")
-local UICorner = Instance.new("UICorner") 
-local EnterKeyButton = Instance.new("TextButton")
-local GetKeyButton = Instance.new("TextButton")
-local BackButton = Instance.new("TextButton")
-local TextBox = Instance.new("TextBox")
+local getkeybutton = Instance.new("TextButton")
+getkeybutton.Name = "getkeybutton"
+getkeybutton.FontFace = Font.new("rbxasset://fonts/families/Sarpanch.json")
+getkeybutton.TextColor3 = Color3.new(1, 1, 1)
+getkeybutton.Text = "get key (noob)"
+getkeybutton.AnchorPoint = Vector2.new(0.5, 0)
+getkeybutton.BackgroundColor3 = Color3.new(0.251, 0.251, 0.251)
+getkeybutton.Position = UDim2.new(0.5, 0, 0.75, 0)
+getkeybutton.BorderSizePixel = 0
+getkeybutton.BorderColor3 = Color3.new()
+getkeybutton.TextSize = 14
+getkeybutton.Size = UDim2.new(0.4, 0, 0.1, 0)
+getkeybutton.Parent = frame
 
-Frame.Parent = ScreenGui
-Frame.Size = UDim2.new(0.4, 0, 0.3, 0) 
-Frame.Position = UDim2.new(0.3, 0, 0.35, 0) 
-Frame.BackgroundColor3 = Color3.fromRGB(169, 169, 169)
+local stroke = Instance.new("UIStroke")
+stroke.Name = "stroke"
+stroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+stroke.LineJoinMode = Enum.LineJoinMode.Miter
+stroke.Color = Color3.new(0.502, 0.502, 0.502)
+stroke.Parent = getkeybutton
 
-UICorner.Parent = Frame
-UICorner.CornerRadius = UDim.new(0, 10)
+local stroke_2 = Instance.new("UIStroke")
+stroke_2.Name = "stroke"
+stroke_2.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+stroke_2.LineJoinMode = Enum.LineJoinMode.Miter
+stroke_2.Color = Color3.new(0, 0.584, 1)
+stroke_2.Thickness = 3
+stroke_2.Parent = frame
 
-EnterKeyButton.Parent = Frame
-EnterKeyButton.Size = UDim2.new(0.4, 0, 0.3, 0)
-EnterKeyButton.Position = UDim2.new(0.05, 0, 0.35, 0) 
-EnterKeyButton.Text = "Enter Key"
-EnterKeyButton.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
-EnterKeyButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-EnterKeyButton.Visible = true  
+local submitbutton = Instance.new("TextButton")
+submitbutton.Name = "submitbutton"
+submitbutton.FontFace = Font.new("rbxasset://fonts/families/Sarpanch.json")
+submitbutton.TextColor3 = Color3.new(1, 1, 1)
+submitbutton.Text = "submit"
+submitbutton.AnchorPoint = Vector2.new(0.5, 0)
+submitbutton.BackgroundColor3 = Color3.new(0.251, 0.251, 0.251)
+submitbutton.Position = UDim2.new(0.5, 0, 0.55, 0)
+submitbutton.BorderSizePixel = 0
+submitbutton.BorderColor3 = Color3.new()
+submitbutton.TextSize = 14
+submitbutton.Size = UDim2.new(0.6, 0, 0.15, 0)
+submitbutton.Parent = frame
 
+local stroke_3 = Instance.new("UIStroke")
+stroke_3.Name = "stroke"
+stroke_3.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+stroke_3.LineJoinMode = Enum.LineJoinMode.Miter
+stroke_3.Color = Color3.new(0.502, 0.502, 0.502)
+stroke_3.Parent = submitbutton
 
-GetKeyButton.Parent = Frame
-GetKeyButton.Size = UDim2.new(0.4, 0, 0.3, 0)
-GetKeyButton.Position = UDim2.new(0.55, 0, 0.35, 0) 
-GetKeyButton.Text = "Get Key"
-GetKeyButton.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
-GetKeyButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-GetKeyButton.Visible = true
+local field = Instance.new("TextBox")
+field.Name = "field"
+field.FontFace = Font.new("rbxasset://fonts/families/Sarpanch.json")
+field.BackgroundColor3 = Color3.new(0.125, 0.125, 0.125)
+field.TextColor3 = Color3.new()
+field.BorderColor3 = Color3.new(0.125, 0.125, 0.125)
+field.Text = ""
+field.TextSize = 14
+field.CursorPosition = -1
+field.AnchorPoint = Vector2.new(0.5, 0)
+field.BorderSizePixel = 0
+field.TextWrapped = true
+field.Position = UDim2.new(0.5, 0, 0.25, 0)
+field.PlaceholderText = "input key"
+field.TextScaled = true
+field.Size = UDim2.new(1, -15, 0.25, 0)
+field.Parent = frame
 
-BackButton.Parent = Frame
-BackButton.Size = UDim2.new(0.2, 0, 0.2, 0)
-BackButton.Position = UDim2.new(0, 0, 0, 0)
-BackButton.Text = "Back"
-BackButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-BackButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-BackButton.Visible = false 
+local stroke_4 = Instance.new("UIStroke")
+stroke_4.Name = "stroke"
+stroke_4.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+stroke_4.LineJoinMode = Enum.LineJoinMode.Miter
+stroke_4.Color = Color3.new(0.251, 0.251, 0.251)
+stroke_4.Parent = field
 
-EnterKeyButton.MouseButton1Click:Connect(function()
-    EnterKeyButton.Visible = false
-    GetKeyButton.Visible = false
+local title = Instance.new("TextLabel")
+title.Name = "title"
+title.FontFace = Font.new("rbxasset://fonts/families/Sarpanch.json")
+title.TextColor3 = Color3.new(0, 0.584, 1)
+title.BorderColor3 = Color3.new()
+title.Text = "key sys"
+title.BackgroundColor3 = Color3.new(1, 1, 1)
+title.BorderSizePixel = 0
+title.BackgroundTransparency = 1
+title.TextScaled = true
+title.TextWrapped = true
+title.TextSize = 14
+title.Size = UDim2.new(1, 0, 0.2, 0)
+title.Parent = frame
 
-    TextBox.Parent = Frame
-    TextBox.Size = UDim2.new(0.8, 0, 0.3, 0)
-    TextBox.Position = UDim2.new(0.1, 0, 0.35, 0)
-    TextBox.PlaceholderText = "Enter your key here"
-    TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
-    TextBox.Visible = true  
+key_ui.Parent = game:GetService('CoreGui')
+local str = game:HttpGet('https://raw.githubusercontent.com/thuarnel/rbx-scripts/refs/heads/main/scripts/rbx-placeholder-keyscript.lua') -- no script yet
+local func = loadstring(func)
 
-    BackButton.Visible = true
-
-    local BackButton2 = Instance.new("TextButton")
-    BackButton2.Parent = Frame
-    BackButton2.Size = UDim2.new(0.2, 0, 0.2, 0)
-    BackButton2.Position = UDim2.new(0, 0, 0, 0)
-    BackButton2.Text = "Back"
-    BackButton2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-    BackButton2.TextColor3 = Color3.fromRGB(255, 255, 255)
-
-    BackButton2.MouseButton1Click:Connect(function()
-        TextBox.Visible = false
-        BackButton2.Visible = false
-        EnterKeyButton.Visible = true
-        GetKeyButton.Visible = true
-        BackButton.Visible = false
-    end)
-end)
-
-GetKeyButton.MouseButton1Up:Connect(function()
-    if type(setclipboard) == 'function' then
-        setclipboard('i farted')
+connect(submitbutton.MouseButton1Up, function()
+    local key = field.Text:lower()
+    local response = func(key)
+    if response ~= true then
+        title.Text = 'wrong key broski'
+        wait(3)
+        title.Text = 'key sys'
     else
-        localplayer:Kick('if your exploit doesnt support setclipboard wtf u doin')
+        title.Text = 'omg key worked ok bye'
+        wait(1)
+        env.stop_voltex_key_system()
     end
 end)
 
